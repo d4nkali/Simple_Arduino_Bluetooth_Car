a5483b40fa6e4aa9765a0e1f1a795538521c63cb
Danilo Pereira <d4n_kali@protonmail.com>
2024-08-07 14:45:42 -0300

    Update Simple_Arduino_Bluetooth_Car.ino

--- Simple_Arduino_Bluetooth_Car/Simple_Arduino_Bluetooth_Car.ino
+++ Simple_Arduino_Bluetooth_Car/Simple_Arduino_Bluetooth_Car.ino
@@ -7,6 +7,41 @@ Data: 07/08/2024 (Primeira Versao: 14/04/2019)
 
 */
 
+/*
+
+# Localização dos pinos no Arduino UNO
+ 
+### Alimentação: 
+
+  - Alimentação +: VIN
+  - Alimentação -: GND
+
+### L298N: 
+
+  - +12: Alimentação +
+  - GND: Alimentação -
+  - INT1: 2
+  - INT2: 3
+  - INT3: 4
+  - INT4: 5
+
+### HC-06 ou HC-05:
+
+  - VCC: 5V
+  - GND: GND
+  - TXD: RX
+  - RXD: Depois do Resistor de 10k e do de 20K
+  - Primeira Perna Resistor de 10k: TX
+  - Segunda Perna Resistor de 20K: GND
+
+#
+
+  **OBS:** Para usar o pino **RX** corretamente deve fazer um **divisor de tensão** para não queimar o modulo. 
+  Então para isso deve colocar o *fio do arduino em um resistor 10k* e na outra ponta *ligar outro resistor de 20 ou 22k*, 
+  no meio deles vai o *fio do sensor* e na outra ponta do de 22k vai um *fio ao GND* da placa.
+
+*/
+
 // Definido os pinos 
 
   const int motor1A = 2; // int1 para o pino 2
